ffecb6f20936793d9b22af48f3840bf3d96c48fe
wg <wg@qq.com>
2019-03-25 20:21:59 +0800

    补充接口

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const bodeParser=require('body-parser');
 const categoryRouter=require('./routes/admin/category')
 const adminRouter=require('./routes/admin/admin')
 const dishRouter=require('./routes/admin/dish')
+const settingsRouter=require('./routes/admin/settings')
+const tableRouter=require('./routes/admin/table')
 
 // 创建HTTP应用服务器
 var app=express()
@@ -23,6 +25,8 @@ app.use(cors());
 app.use(bodeParser.json()) //把JSON格式的请求主体数据解析出来放入req.body
 
 // 挂载路由器
-app.use('/admin/category',categoryRouter); 
+app.use('/admin/category',categoryRouter)
 app.use('/admin',adminRouter)
 app.use('/admin/dish',dishRouter)
+app.use('/admin/settings',settingsRouter)
+app.use('/admin/table',tableRouter)
